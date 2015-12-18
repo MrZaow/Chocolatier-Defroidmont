eb7b7f3e4667e1226847e5522b508647f9a205b5
MrZaow <misterzaow@gmail.com>
2015-12-18 19:36:09 +0100

    update1

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="logo_chocolatier.png" alt="logo"></a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="../pages/produits.php">Produits</a></li>
+                <li><a href="../pages/noustrouver.php">Où nous trouver</a></li>
+                <li><a href="#">Musée</a></li>
+                <li><a href="#">Équipe</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Infos pratiques</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" style="pointer-events: none; cursor: default; "><strong> Téléphone : <i class="icon-phone"></i> 086/21.84.40</strong></a></li>
+          </ul>
+
+        </div>
+    </div>
+</header><!--/header-->
--- index.php
+++ index.php
@@ -38,11 +38,12 @@
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
 
-                    <li><a href="index.html">Accueil</a></li>
-                    <li><a href="#">Nos boutiques</a></li>
-                    <li><a href="#">Le musée</a></li>
-                    <li><a href="#">Points de ventes</a></li>
-                    <li><a href="#">Achats en ligne</a></li>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="pages/produits.php">Produits</a></li>
+                    <li><a href="pages/noustrouver.php">Où nous trouver</a></li>
+                    <li><a href="#">Musée</a></li>
+                    <li><a href="#">Équipe</a></li>
+                    <li><a href="#">Blog</a></li>
                     <li><a href="#">Infos pratiques</a></li>
                 </ul>
 
@@ -296,7 +297,87 @@
         </div>
     </section><!--/#testimonial-->
 
-    <?php $include->footer(); ?>
+    <section id="bottom" class="wet-asphalt">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <h4>A propos de nous</h4>
+                    <p>Situé à l’entrée du stade de Bielmont, à 2 minutes en voiture du Palais de Justice et du grand hôpital (CHPLT), desservi par les bus 294, 295, 390, 393, 395, 705 et 708, le centre vétérinaire vous accueille du lundi matin au samedi après-midi. </p>
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Vétérinaire Abyssin</h4>
+                    <div>
+                        <ul class="arrow">
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="#">Équipe</a></li>
+                            <li><a href="#">Visite guidée</a></li>
+                            <li><a href="#">Articles</a></li>
+                            <li><a href="#">Vos animaux</a></li>
+                            <li><a href="#">Infos pratiques</a></li>
+                        </ul>
+                    </div>
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Articles récents</h4>
+                    <div>
+                        <div class="media">
+                            <div class="pull-left">
+                                <img src="images/blog/thumb1.jpg" alt="">
+                            </div>
+                            <div class="media-body">
+                                <span class="media-heading"><a href="#">Pellentesque habitant morbi tristique senectus</a></span>
+                                <small class="muted">Posted 17 Aug 2013</small>
+                            </div>
+                        </div>
+                        <div class="media">
+                            <div class="pull-left">
+                                <img src="images/blog/thumb2.jpg" alt="">
+                            </div>
+                            <div class="media-body">
+                                <span class="media-heading"><a href="#">Pellentesque habitant morbi tristique senectus</a></span>
+                                <small class="muted">Posted 13 Sep 2013</small>
+                            </div>
+                        </div>
+                        <div class="media">
+                            <div class="pull-left">
+                                <img src="images/blog/thumb3.jpg" alt="">
+                            </div>
+                            <div class="media-body">
+                                <span class="media-heading"><a href="#">Pellentesque habitant morbi tristique senectus</a></span>
+                                <small class="muted">Posted 11 Jul 2013</small>
+                            </div>
+                        </div>
+                    </div>  
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Adresse</h4>
+                    <address>
+                        <p>Centre Vétérinaire Abyssin</p>
+                        <p>Place d'Arles 4, 4800 Verviers, Belgique</p>
+                        <p>087 23 00 23</p>
+                    </address>
+                </div> <!--/.col-md-3-->
+            </div>
+        </div>
+    </section><!--/#bottom-->
+
+    <footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                 Site réalisé par : <a target="_blank" href="http://webinpres.com/" title="Conception de sites web">Webinpres</a>.
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i> Haut de page</a></li><!--#gototop-->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
 
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
