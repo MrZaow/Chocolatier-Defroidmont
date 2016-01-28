627e6d3735e931568fa8a670ccb1df70bea8b065
robin lespagnard <misterzaow@gmail.com>
2016-01-28 13:56:58 +0100

    Page commander

---
+++ pages/fr/commander.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include("../../includes/head.php") ?>
+<body>
+    <?php include("../../includes/fr/header.php") ?>
+
+    <section id="title" class="orange">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>Commander nos produits</h1>
+                </div>
+            </div>
+        </div>
+    </section><!--/#title-->    
+
+    <section class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Comment commander nos produits en ligne ?</h2>
+                <p>Pour commander nos produits en ligne, rien de plus simple ! Contactez-nous à l'adresse mail
+                 <a href="mailto:contact@chocolatier-defroidmont.be"> contact@chocolatier-defroidmont.be</a> en précisant les produits et les quantités souhaitées,
+                 ainsi que vos coordonnées et l'adresse à laquelle vous souhaitez être livré. Nous vous enverrons votre facture et, après réception de votre paiement,
+                 nous livrerons vos produits.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="../../images/equipe-defroidmont.jpg" alt="Équipe de la chocolaterie Defroidmont" class="img-responsive">
+            </div>
+        </div>
+    </section>
+
+    <?php include("../../includes/fr/bottom.php") ?>
+
+    <?php include("../../includes/fr/footer.php") ?>
+
+    <?php include("../../includes/script.php") ?>
+</body>
+</html>
